418374e195149c7c87721a4837eb12d86b644765
Anna-14-pol <anna.mucha.1401@gmail.com>
2020-06-24 18:05:11 +0200

    dropdowns

--- src/components/Header/languages.jsx
+++ src/components/Header/languages.jsx
@@ -1,40 +1,60 @@
 import React from 'react';
-import { Flag, Segment } from 'semantic-ui-react';
+import { Dropdown } from 'semantic-ui-react';
 
 // const languages = ['pl','pt','us','ro'];
-class LanguagesDrop extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          value: '',
-        };
-      }
-    
-      onValueChange =(event) =>{
-        const value = event.target.value;
-        this.setState({value});
-        this.props.onLanguageChange(value);
-      }
-    
-      render() {
-        return (
-         
-          <Segment value={this.state.value} onChange={this.onValueChange}>
-          <Flag name='pl' value='pl' />
-          <Flag name='pt' value='pt' />
-          <Flag name='us' value='us' />
-          <Flag name='ro' value='ro' />
-          </Segment>
-        
-        );
-      }};
+
+const countryOptions = [
+  { key: 'pl', value: 'pl', flag: 'pl', text: 'Poland' },
+  { key: 'pt', value: 'pt', flag: 'pt', text: 'Portugal' },
+  { key: 'us', value: 'us', flag: 'us', text: 'USA' },
+  { key: 'ro', value: 'ro', flag: 'ro', text: 'Romania' },
+]
+const LanguagesDrop = ({ language, onLanguageChange}) => (
+  <Dropdown
+  placeholder='Select Country'
+  fluid
+  search
+  selection
+  options={countryOptions}
+  onChange={(event, data) => onLanguageChange(data.value)}
+/>
+
+);
+ 
 
 export default LanguagesDrop;
 
 
 
-{/* <select value={this.state.value} onChange={this.onValueChange}>
-                  {languages.map((lang) =>(
-                      <option key={lang} value={lang}>{lang}</option>
-                  ))}
-              </select> */}
+
+
+//     constructor(props) {
+//         super(props);
+//         this.state = {
+//           value: '',
+//         };
+//       }
+    
+//       onValueChange =(event) =>{
+//         const value = event.target.value;
+//         this.setState({value});
+//         this.props.onLanguageChange(value);
+//       }
+    
+//       render() {
+//         return (
+  // <select value={this.state.value} onChange={this.onValueChange}>
+  // //                   {languages.map((lang) =>(
+  // //                       <option key={lang} value={lang}>{lang}</option>
+  // //                   ))}
+  // //               </select> 
+//         
+        
+//         );
+//       }};
+
+// export default LanguagesDrop;
+
+
+
+//  
--- src/components/Main/HomePage/NewsFiltersBar/category.jsx
+++ src/components/Main/HomePage/NewsFiltersBar/category.jsx
@@ -1,32 +1,62 @@
 import React from 'react';
+import { Dropdown } from 'semantic-ui-react';
 
-class CategoryDrop extends React.Component{
-    constructor(props) {
-        super(props);
-        this.state = {
-          value: '',
-        };
-      }
-    
-      handleChange =(event) =>{
-        const value = event.target.value;
-        this.setState({value});
-        this.props.onCategoryChange(value);
-      }
-    
-      render() {
-        return (
-              <select value={this.state.value} onChange={this.handleChange}>
-                <option value='general'>ogólna</option>  
-                <option value="business">biznes</option>
-                <option value="entertainment">rozrywka</option>
-                <option value="health">zdrowie</option>
-                <option value="science">nauka</option>
-                <option value="sport">sport</option>
-                <option value="technology">technologia</option>
-              </select>
-        );
-      }
-};
+const options = [
+  { key: 1, text: 'general', value: 'general' },
+  { key: 2, text: 'business', value: 'business' },
+  { key: 3, text: 'entertainment', value: 'entertainment' },
+  { key: 4, text: 'health', value: 'health' },
+  { key: 5, text: 'science', value: 'science' },
+  { key: 6, text: 'sport', value: 'sport' },
+  { key: 7, text: 'technology', value: 'technology' },
+]
+
+const CategoryDrop = ({ category, onCategoryChange }) => (
+ 
+  <Dropdown
+    placeholder="Select category"
+    options={options}
+    selection
+    onChange={(event, data) => onCategoryChange(data.value)}
+  />
+);
 
 export default CategoryDrop;
+
+
+
+
+
+    // constructor(props) {
+    //     super(props);
+    //     this.state = {
+    //       value: '',
+    //     };
+    //   }
+      
+    
+    //   handleChange =(e, v) =>{
+    //     changeMethod(e, v)
+    //     const value = e.target.value;
+    //     this.setState({value});
+    //     this.props.onCategoryChange(value);
+      
+    //   }
+    
+    //   render() {
+    //     return (
+            
+
+              // <select value={this.state.value} onChange={this.handleChange}>
+              //   <option value='general'>ogólna</option>  
+              //   <option value="business">biznes</option>
+              //   <option value="entertainment">rozrywka</option>
+              //   <option value="health">zdrowie</option>
+              //   <option value="science">nauka</option>
+              //   <option value="sport">sport</option>
+              //   <option value="technology">technologia</option>
+              // </select>
+    //     );
+    //   }
+    // };
+
